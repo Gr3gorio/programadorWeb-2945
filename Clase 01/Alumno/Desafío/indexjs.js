37ba840d09cfd68f53c21c685d67db957bb9aff8
Gr3gorio <gregorio.vargas@gmail.com>
2018-02-17 13:51:33 -0300

    desafio clase01

---
+++ Clase 01/Alumno/Desafío/indexjs.js
@@ -0,0 +1,10 @@
+var Number1 = prompt ('ingrese un primer numero')
+var parsedNumber1 = parseInt (Number1 , 10)
+
+var Number2 = prompt ('ingrese un segundo numero')
+var parsedNumber2 = parseInt (Number2 , 10)
+
+console.log('la suma es : ', parsedNumber1 + parsedNumber2)
+console.log('la resta es : ', parsedNumber1 - parsedNumber2)
+console.log('la multiplicacion es : ', parsedNumber1 * parsedNumber2)
+console.log('la division es : ', parsedNumber1 / parsedNumber2)
---
+++ Clase 01/Alumno/Ejercicio/indexjs.js
@@ -0,0 +1,25 @@
+var nombre = prompt('Ingrese su nombre')
+console.log ('hola'+' '+nombre)
+
+
+var Number2 = 20
+var Number1 = prompt ('ingrese un numero')
+var parsedNumber1 = parseInt (Number1 , 10)
+
+console.log('la suma es : ', parsedNumber1 + Number2)
+
+var Number3 = 20
+var Number4 = prompt ('ingrese un numero')
+var parsedNumber4 = parseInt (Number4 , 10)
+console.log( 'la resta es : ',Number3 -parsedNumber4 )
+
+
+var Number7 = prompt ('ingrese un primer numero')
+var parsedNumber7 = parseInt (Number7 , 10)
+
+var Number8 = prompt ('ingrese un segundo numero')
+var parsedNumber8 = parseInt (Number8 , 10)
+
+console.log('la suma es : ', parsedNumber7 + parsedNumber8)
+
+
